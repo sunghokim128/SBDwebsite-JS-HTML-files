e9b4dd47a376f3bbfdb79a022687edd636b163bd
Sungho Kim <92619529+sunghokim128@users.noreply.github.com>
2022-08-30 10:05:42 +0900

    Add files via upload

--- FrontEnd/postingAlert.js
+++ FrontEnd/postingAlert.js
@@ -20,17 +20,21 @@ function alertMessage() {
     } else if( !/^[0-9]+$/.test(squat) || !/^[0-9]+$/.test(benchpress) || !/^[0-9]+$/.test(deadlift) || !/^[0-9]+$/.test(weight)){
       alert("숫자만 입력해주시기 바랍니다");
       return false;
+    } else if( title.length > 100 ){
+      alert("제목이 너무 깁니다. 100자 아래로 입력해주세요");
+      return false;
+    } else if( body.length > 1000 ){
+      alert("본문이 너무 큽니다. 1000자 아래로 입력해주세요");
+      return false;
     } else
-    {
-         if (confirm("글을 한번 게시하면 수정/삭제가 불가능합니다\n그래도 글을 게시하시겠습니까?") ) {
-           //[확인] 누르면 글을 게시하고 메인 페이지로 돌아감
-               return true;
-              } else {
-              //[취소] 누르면 경고 메세지 뜨기 전 상태로 돌아감
-              return false;
-            }
-    }
 
+    {
+        if(confirm(returnScript)){
+          if (confirm("글을 한번 게시하면 수정/삭제가 불가능합니다\n그래도 글을 게시하시겠습니까?") ) { return true; }
+          else { return false; }
+        }
+        else{ return false; }
+    }
  }
 
  function BSChecker() {
@@ -41,7 +45,7 @@ function alertMessage() {
      var weight = parseInt(document.getElementById("weight").value);
 
      if( weight < 30 ){
-        alert("쇠질을 할 때가 아닙니다. 밥을 먹으러 가십시오");
+        alert("쇠질을 할 때가 아닙니다. 밥을 먹을 때입니다.");
         return false;
      }else if(benchpress > 355){
         alert("현재 벤치프레스 세계 신기록은 355kg으로 Julius Maddox가 보유하고 있습니다. \n 당신의 벤치기록은 지금 입력한 그것이 아닙니다."
